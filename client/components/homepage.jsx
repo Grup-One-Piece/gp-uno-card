2d562d43ee46ee9e0a0b4858964c6603f35da2b9
Syarah Namira <syarahnamira@gmail.com>
2024-03-20 00:49:54 +0700

    Feat: create homepage and styling css

--- client/components/homepage.jsx
+++ client/components/homepage.jsx
@@ -1,7 +1,35 @@
-import React from "react"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
-function Homepage(){
+function Homepage() {
+  const [roomCode, setRoomCode] = useState("");
 
+  return (
+    <div className="Homepage">
+      <div className="homepage-menu">
+        <img src={"/logo.png"} width="200px" />
+        <div className="homepage-form">
+          <div className="homepage-join">
+            <input
+              type="text"
+              placeholder="Game Code"
+              onChange={(event) => setRoomCode(event.target.value)}
+            />
+            <Link to={`/play?roomCode=${roomCode}`}>
+              <button className="game-button green">JOIN GAME</button>
+            </Link>
+          </div>
+          <h1>OR</h1>
+          <div className="homepage-create">
+            <Link to={`/play?roomCode=${uuidv4()}`}>
+              <button className="game-button orange">CREATE GAME</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
